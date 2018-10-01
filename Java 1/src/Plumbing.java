283ad5ada7440609baa74eec1ba2e732c88f0619
Jacob Baker <hpv212@mocs.utc.edu>
2018-10-01 17:52:06 -0400

    added great.java love.java name.java plumbing.java and robot.java

---
+++ Java 1/src/Plumbing.java
@@ -0,0 +1,14 @@
+/**
+ * This is the plumbing needed for each and every main class.
+ *
+ * @author (Jacob Baker)
+ * @version (8/29/2018)
+ */
+public class Plumbing
+{
+    public static void main(String[] arg)
+    {
+        
+        
+    }
+}
