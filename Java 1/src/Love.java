37fb709ce5b552fee8547b00252dc7755d1b2000
Jacob Baker <hpv212@mocs.utc.edu>
2018-10-01 17:54:25 -0400

    Love.java

--- Java 1/src/Love.java
+++ Java 1/src/Love.java
@@ -1,4 +1,15 @@
-
-public class Love {
-
+/**
+ * This is a great class that prints out I love programming.
+ * 
+ * @author (Jacob Baker)
+ * @version (8/29/2018)
+ */
+public class Love
+{
+        public static void main(String[] arg)
+    
+        {
+            System.out.println("CPSC is great");
+            System.out.println("I Love Programming");
+        }
 }
