448581118aecac5dd09b678b7e61094c22b05eea
Jacob Baker <hpv212@mocs.utc.edu>
2018-10-01 18:33:55 -0400

    added mystring.java

---
+++ Java 2/src/MyString.java
@@ -0,0 +1,18 @@
+
+/**
+ * String Concat.
+ *
+ * @author (Jacob Baker)
+ * @version (9/7/2018)
+ */
+public class MyString
+{ 
+    public static void main(String[] arg){
+        String firstname = " Jacob " + "";
+        String lastname = " Baker" + ".";
+        String Blank = "";
+        String fullname = "Jacob Baker";
+        System.out.println(firstname + lastname);
+     }
+}
+
