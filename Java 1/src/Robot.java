82ab4ee52bafa72f5ebe1192598a65c31a92f885
Jacob Baker <hpv212@mocs.utc.edu>
2018-10-01 17:55:24 -0400

    robot.java commit

--- Java 1/src/Robot.java
+++ Java 1/src/Robot.java
@@ -1,4 +1,19 @@
-
-public class Robot {
-
+/**
+ * This is a great class that prints out a robot.
+ * 
+ * @author (Jacob Baker)
+ * @version (8/29/2018)
+ */
+public class Robot
+{
+    public static void main(String[] arg)
+    {
+        System.out.println("--------");
+        System.out.println(" | 0  0 |");
+        System.out.println("(|  ^  |)");
+        System.out.println(" | [ ] |");
+        System.out.println("---------");
+    }
 }
+        
+    
