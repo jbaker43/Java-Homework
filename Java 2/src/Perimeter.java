b046feb21b82019aa352561b1be82aa6305db1b1
Jacob Baker <hpv212@mocs.utc.edu>
2018-10-01 17:59:24 -0400

    added perimeter.java

---
+++ Java 2/src/Perimeter.java
@@ -0,0 +1,29 @@
+
+/**
+ * This is a program that computes perimeter of a rectangle.
+ *
+ * @author (Jacob Baker)
+ * @version (9/7/2018)
+ */
+import java.awt.Rectangle;
+public class Perimeter
+{
+    public static void main(String[] arg) 
+    {
+  { 
+      Rectangle box = new Rectangle(10,25,35,35);
+      System.out.println(new Rectangle());
+      String name = "Width";
+      System.out.println(box.getWidth());
+      System.out.println("Width" + box.getWidth());
+      String nameh = "Height";
+      System.out.println(box.getHeight());
+      System.out.println("Height" + box.getHeight());
+      int Width = 35;
+      int Height = 25;
+      int perimeter = 2 * (Height + Width);
+      System.out.println("perimeter" + perimeter);
+      System.out.println("Expected: 120");
+  }
+}
+}
