dea1c11b443e3fcb176146ebaf350c17419f35f4
Jacob Baker <hpv212@mocs.utc.edu>
2018-10-01 17:54:44 -0400

    name.java commit

--- Java 1/src/Name.java
+++ Java 1/src/Name.java
@@ -1,4 +1,16 @@
 
-public class Name {
-
+/**
+ * Write a description of class Name here.
+ *
+ * @author (Jacob Baker)
+ * @version (8/29/2018)
+ */
+public class Name
+{
+    public static void main (String[]arg)
+    {
+        System.out.println("CPSC is great");
+        System.out.println("I Love Programming");
+        System.out.println("Jacob Baker");
+    }
 }
