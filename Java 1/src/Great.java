1124a3f2d89c3b8b7410ce8d23b3d12cffa8f287
Jacob Baker <hpv212@mocs.utc.edu>
2018-10-01 17:53:13 -0400

    great.java added

--- Java 1/src/Great.java
+++ Java 1/src/Great.java
@@ -1,4 +1,14 @@
-
-public class Great {
-
+/**
+ * This is a great class that prints out CPSC is great class.
+ * 
+ * @author (Jacob Baker)
+ * @version (8/29/2018)
+ */
+public class Great
+{
+        public static void main(String[] arg)
+    
+        {
+            System.out.println("4+6");
+        }
 }
