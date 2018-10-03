a833a9e46cdb3ad53e87ccad6cfc4ff8dee1f5d0
Jacob Baker <hpv212@mocs.utc.edu>
2018-10-03 11:21:46 -0400

    Added bug.java

---
+++ Assignment 4/src/Bug.java
@@ -0,0 +1,30 @@
+/**
+ * reports number of bugs
+ * @jakebaker
+ * 09/04/2018
+ */
+public class Bug {
+
+    private int numberOfBugs;
+
+    public Bug(int numBugs)
+    {
+        numberOfBugs = numBugs;
+    }
+
+    public void breedBugs()
+    {
+        numberOfBugs = 2*numberOfBugs;
+    }
+
+    public void sprayBugs()
+    {
+        numberOfBugs = (int)(numberOfBugs * 0.75);
+    }
+
+    public int getNumberOfBugs()
+    {
+        return numberOfBugs;
+    }
+
+}
